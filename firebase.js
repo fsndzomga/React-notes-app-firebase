501cd7dcfedfd9cf601c4eac84e045f4bdc45bdd
Franck Stéphane Ndzomga <101533724+fsndzomga@users.noreply.github.com>
2023-05-08 20:20:49 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, collection } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKP6MO8LWpH0dMzuHeVv7Vi86_p6RNYpg",
-  authDomain: "react-notes-app-79994.firebaseapp.com",
-  projectId: "react-notes-app-79994",
-  storageBucket: "react-notes-app-79994.appspot.com",
-  messagingSenderId: "26909206260",
-  appId: "1:26909206260:web:547935b6fe8ade3327e0a1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
